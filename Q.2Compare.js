71bc06c7385fc10d546bdb99662e0e7981d8e0a6
Bhuvijain230 <bhuvijain230@gmail.com>
2024-07-25 22:11:08 +0530

    Comparison

---
+++ Q.2Compare.js
@@ -0,0 +1,16 @@
+//using if-else
+let x=3,y=8;
+let max;
+if(x>y){
+   max=x;
+}
+else{
+    max=y;
+}
+console.log("max1=",max);
+//using ternary operator
+let a=20,b=40;
+let max2=(a>b)?a:b;
+console.log("max2=",max2);
+
+
